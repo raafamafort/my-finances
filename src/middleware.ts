d7704243e7d8291fee32a53d4f4fa41f4284f3dc
raafamafort <rafaelmafortc@gmail.com>
2024-08-05 23:35:16 -0300

    fix: middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,5 +30,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/resume', '/income', '/expense', '/sign-in', '/sign-up', '/'],
+  matcher: ['/:path*'],
 };
